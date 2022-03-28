1522b09027ea03323c500e1c8efc70f58f07fdca
BrendaCheyech <brendacheyechy@gmail.com>
2022-03-28 10:02:36 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - updates a integer value to 98
+ *@n: a pointer to an int
+ *
+ *return: void that means it is correct
+ */
+
+void reset_to_98(int *n)
+
+}
+*n = 98;
+}

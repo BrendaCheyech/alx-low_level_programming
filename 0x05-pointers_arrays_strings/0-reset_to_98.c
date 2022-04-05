9d6ee55239bc6aeed9d24963641088d7d5874001
BrendaCheyech <brendacheyechy@gmail.com>
2022-04-05 09:25:30 -0700

    Task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,11 +3,10 @@
 /**
  *reset_to_98 - updates a integer value to 98
  *@n: a pointer to an int
- *
  *return: void that means it is correct
  */
 
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
  *n = 98;
 }

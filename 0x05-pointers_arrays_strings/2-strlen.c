c64c58cfa085ba1f2f4b5b6b66ea518666c78805
BrendaCheyech <brendacheyechy@gmail.com>
2022-04-05 09:58:47 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <main.h>
+/**
+ * _strlen - length of string
+ * @s: is a pointer int char type
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+int i;
+
+for (i = 0; *s != '\0'; i++)
+{
+continue;
+}
+return (i);
+}
